c3fe462e61ed7faa6b3f152376915308c1ce5ef3
exit <chungjung.d@gmail.com>
2023-09-10 00:29:58 +0900

    ✨ Chat query type

--- x/eywa/client/cli/query.go
+++ x/eywa/client/cli/query.go
@@ -31,6 +31,8 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 
 	cmd.AddCommand(CmdGetRelayServer())
 
+	cmd.AddCommand(CmdGetChat())
+
 	// this line is used by starport scaffolding # 1
 
 	return cmd
---
+++ x/eywa/client/cli/query_get_chat.go
@@ -0,0 +1,52 @@
+package cli
+
+import (
+	"strconv"
+
+	"eywa/x/eywa/types"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+)
+
+var _ = strconv.Itoa(0)
+
+func CmdGetChat() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "get-chat [room-id]",
+		Short: "Query get-chat",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			reqRoomId := args[0]
+
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			params := &types.QueryGetChatRequest{
+
+				RoomId: reqRoomId,
+			}
+
+			pageReq, err := client.ReadPageRequest(cmd.Flags())
+			if err != nil {
+				return err
+			}
+			params.Pagination = pageReq
+
+			res, err := queryClient.GetChat(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
