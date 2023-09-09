99b5cc97651157a191ccf7944c136132b8ae094b
exit <chungjung.d@gmail.com>
2023-09-09 23:05:11 +0900

    ✨ Query find all relay server

--- x/eywa/keeper/query_get_relay_server.go
+++ x/eywa/keeper/query_get_relay_server.go
@@ -4,7 +4,10 @@ import (
 	"context"
 
 	"eywa/x/eywa/types"
+
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -14,10 +17,26 @@ func (k Keeper) GetRelayServer(goCtx context.Context, req *types.QueryGetRelaySe
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 
+	var relayServers []types.RelayServer
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Process the query
-	_ = ctx
+	store := ctx.KVStore(k.storeKey)
+	relayServerStore := prefix.NewStore(store, types.KeyPrefix(types.RelayServerKey))
 
-	return &types.QueryGetRelayServerResponse{}, nil
+	pageRes, err := query.Paginate(relayServerStore, req.Pagination, func(key []byte, value []byte) error {
+		var relayServer types.RelayServer
+		if err := k.cdc.Unmarshal(value, &relayServer); err != nil {
+			return err
+		}
+
+		relayServers = append(relayServers, relayServer)
+
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, "not found")
+	}
+
+	return &types.QueryGetRelayServerResponse{RelayServer: relayServers, Pagination: pageRes}, nil
 }
