546abd58b3a8f8f6c67d08ebb4f02cf2376b04df
exit <chungjung.d@gmail.com>
2023-09-09 22:11:49 +0900

    👔 Change index of handshake

--- x/eywa/keeper/handshake.go
+++ x/eywa/keeper/handshake.go
@@ -11,7 +11,7 @@ func (k Keeper) CreateHandshakeService(ctx sdk.Context, handshake types.Handshak
 	handshake.HashId = k.GetHandshakeHashId(ctx, handshake)
 	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.HandshakeKey))
 	appendedValue := k.cdc.MustMarshal(&handshake)
-	store.Set(GetHandshakeReceiverBytes(handshake.Receiver), appendedValue)
+	store.Set(GetHandshakeHashIdByte(handshake.HashId), appendedValue)
 	return handshake.HashId
 }
 
@@ -19,6 +19,6 @@ func (k Keeper) GetHandshakeHashId(ctx sdk.Context, handshake types.Handshake) s
 	return handshake.Sender + handshake.Receiver
 }
 
-func GetHandshakeReceiverBytes(receiver string) []byte {
-	return []byte(receiver)
+func GetHandshakeHashIdByte(hashId string) []byte {
+	return []byte(hashId)
 }
