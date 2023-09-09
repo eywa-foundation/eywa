2677179266c1cc6498be8b824ab0af126c2d639b
exit <chungjung.d@gmail.com>
2023-09-10 00:35:48 +0900

    👔 Add query busniss logic

--- x/eywa/keeper/query_get_chat.go
+++ x/eywa/keeper/query_get_chat.go
@@ -4,7 +4,10 @@ import (
 	"context"
 
 	"eywa/x/eywa/types"
+
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -14,10 +17,27 @@ func (k Keeper) GetChat(goCtx context.Context, req *types.QueryGetChatRequest) (
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 
+	var chats []types.Chat
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Process the query
-	_ = ctx
+	store := ctx.KVStore(k.storeKey)
+	chatStore := prefix.NewStore(store, types.KeyPrefix(types.ChatKey))
 
-	return &types.QueryGetChatResponse{}, nil
+	pageRes, err := query.Paginate(chatStore, req.Pagination, func(key []byte, value []byte) error {
+		var chat types.Chat
+		if err := k.cdc.Unmarshal(value, &chat); err != nil {
+			return err
+		}
+
+		//if req.Receiver == handshake.Receiver
+		if req.RoomId == chat.RoomId {
+			chats = append(chats, chat)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.InvalidArgument, "not found")
+	}
+	return &types.QueryGetChatResponse{Chat: chats, Pagination: pageRes}, nil
 }
