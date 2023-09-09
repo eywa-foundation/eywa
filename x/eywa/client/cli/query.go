c3d1e79125884f218d317157c44185d678d52520
exit <chungjung.d@gmail.com>
2023-09-09 22:58:09 +0900

    ✨ Add query achitecute

--- x/eywa/client/cli/query.go
+++ x/eywa/client/cli/query.go
@@ -29,6 +29,8 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 
 	cmd.AddCommand(CmdGetHandshake())
 
+	cmd.AddCommand(CmdGetRelayServer())
+
 	// this line is used by starport scaffolding # 1
 
 	return cmd
